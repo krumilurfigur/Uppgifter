e62a482ad6a874747db11903523979ab4c25bf1b
krumilurfigur <gabrieloberg3@gmail.com>
2019-10-18 11:14:48 +0200

    finished gissa talet

---
+++ src/annat/gissatalet/test.java
@@ -0,0 +1,19 @@
+/*
+a program that let's you guess a number
+2019-10-17
+Gabriel Öberg
+ */
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int r = (int) (Math.random() * 100 + 1);
+        int n = 0;
+        int i = 0;
+
+        System.out.println(r);
+    }
+}
